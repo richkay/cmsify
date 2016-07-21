47be5c517d161fb4cd92acf0537b66756c2ec583
Stefan Neuser <mail@stefan-neuser.eu>
2016-07-21 16:08:34 +0200

    change stylings, add keywords to composer.json

--- src/assets/js/cmsify.js
+++ src/assets/js/cmsify.js
@@ -38,7 +38,7 @@ if (module.hot) {(function () {  module.hot.accept()
 })()}
 },{"vue":114,"vue-hot-reload-api":110}],3:[function(require,module,exports){
 var __vueify_insert__ = require("vueify/lib/insert-css")
-var __vueify_style__ = __vueify_insert__.insert("\n\nul {\n    font-size: 15px;\n    list-style: none;\n    padding: 3px;\n    border-left: 1px solid #ccc;\n    padding-left: 10px;\n}\n\n")
+var __vueify_style__ = __vueify_insert__.insert("\n\nul {\n    font-size: 15px;\n    list-style: none;\n    padding-right: 3px;\n    border-left: 1px solid #ccc;\n    padding-left: 10px;\n}\n\nul > li {\n    padding:0px;\n    margin:0px;\n}\n\n")
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -172,7 +172,7 @@ if (module.hot) {(function () {  module.hot.accept()
   hotAPI.install(require("vue"), true)
   if (!hotAPI.compatible) return
   module.hot.dispose(function () {
-    __vueify_insert__.cache["\n\nul {\n    font-size: 15px;\n    list-style: none;\n    padding: 3px;\n    border-left: 1px solid #ccc;\n    padding-left: 10px;\n}\n\n"] = false
+    __vueify_insert__.cache["\n\nul {\n    font-size: 15px;\n    list-style: none;\n    padding-right: 3px;\n    border-left: 1px solid #ccc;\n    padding-left: 10px;\n}\n\nul > li {\n    padding:0px;\n    margin:0px;\n}\n\n"] = false
     document.head.removeChild(__vueify_style__)
   })
   if (!module.hot.data) {
